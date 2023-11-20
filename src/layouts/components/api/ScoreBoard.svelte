0cbd113e5bb1ffe1a3e82a86a192f64b9e06c6aa
ezriharmusial <ezriharmusial@gmail.com>
2023-11-20 06:29:56 +0100

    Api Updates

---
+++ src/layouts/components/api/ScoreBoard.svelte
@@ -0,0 +1,23 @@
+<script>
+export const board = "vault_eco_balance"
+export const type = "alltime"
+export const size = 5
+import ScoreRow from "./ScoreRow.svelte"
+
+</script>
+
+<article class="card card-hover variant-filled-primary bg-black/10 rounded-md p-4 my-4">
+    <header class="card-header">
+        <h3>Top {size} {board}'s of {type}</h3>
+    </header>
+    <main>
+        <dl class="divide-y divide-gray-100">
+          {#each {length: 5} as _, i}
+          <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <ScoreRow board={board} type={type} position={i+1} />
+          </div>
+          {/each}
+        </dl>
+    </main>
+    <footer class="card-footer"></footer>
+</article>
---
+++ src/layouts/components/api/ScoreRow.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+
+  export let board = "vault_eco_balance"
+  export let type = "alltime"
+  export let position = "1"
+  export let format = "score"
+
+  const apiData = writable({
+    "playerName": "---",
+    "playerDisplayName": "---",
+    "playerPrefix": "",
+    "playerSuffix": "",
+    "playerUUID": "null",
+    "position": position,
+    "board": board,
+    "type": type,
+    "score": -1.0,
+    "scorePretty": "---",
+    "scoreFormatted": "---",
+    "scoreTime": "---"
+  })
+
+  onMount(async () => {
+    fetch(`https://leaderboards.highstreet.fun/${board}/${type}/${position}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      apiData.set(data);
+    }).catch(error => {
+      console.log(error);
+      return [];
+    });
+  });
+</script>
+
+<dt class="text-sm font-bold leading-6 text-gray-900 flex justify-left items-center">
+  {position}. <img
+  class="group-hover:scale-[1.03] transition duration-300 w-8 mx-2"
+  src={"https://mc-heads.net/head/" + $apiData.playerName}
+  alt={$apiData.playerUUID}
+  width={48}
+  height={48}
+  transition:name={"image" + $apiData.playerName}
+  /> {$apiData.playerPrefix} {$apiData.playerDisplayName || $apiData.playerName} {$apiData.playerSuffix}
+</dt>
+<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 flex justify-center items-center">
+  {#if format == "score"}{$apiData.score}
+  {:else if format == "scorePretty"}{$apiData.scorePretty}
+  {:else if format == "scoreFormatted"}{$apiData.scoreFormatted}
+  {:else if format == "scoreTime"}{$apiData.scoreTime}{/if}
+</dd>
